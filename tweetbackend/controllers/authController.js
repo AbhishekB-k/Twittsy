56696b53460bfed62f2f2d249f8f72baddac3408
AbhishekB-k <abhi37134@gmail.com>
2024-08-16 22:24:53 +0530

    db-setup

---
+++ tweetbackend/controllers/authController.js
@@ -0,0 +1,17 @@
+export const signup = async(req,res)=>{
+    res.json({
+        data:"Sign Up",
+    });
+};
+
+export const login = async(req,res)=>{
+    res.json({
+        data:"Log In",
+    });
+};
+
+export const logout = async(req,res)=>{
+    res.json({
+        data:"Log Out",
+    });
+};
